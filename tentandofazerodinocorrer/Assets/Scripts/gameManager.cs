a7b78b7163ac112379d73e276a0d9d0aa1b6a6bf
semimarufbi <gabicolzani@gmail.com>
2024-07-29 10:01:27 -0300

    gameManager
    oloco

---
+++ tentandofazerodinocorrer/Assets/Scripts/gameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    float speed = 4;
+    int score;
+    Vector2 screenBounds;
+
+    public float Speed { get => speed;  }
+    public int Score { get => score; }
+    public Vector2 ScreenBounds { get => screenBounds;  }
+    #region sigleton
+    private void Awake()
+    {
+        instance = this;
+        screenBounds = new Vector3(1, 0, 0) + Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
